f5e1cdeccf0ca1042252146132389bb66a514cb2
KirkZaugg <25kzaugg@go.dsdmail.net>
2024-10-15 14:03:46 +0000

    finished instruction decoding

--- Control.cpp
+++ Control.cpp
@@ -61,66 +61,117 @@ void Control::operate() {
     
         inputreg.setValue(ram->getValue(pc->getWholeValue()));
 
-        
+        switch (inputreg.getValue()) {
+            case 0x00: //BRK
+            case 0x20: //JSR abs
+            case 0x40: //RTI
+            case 0x60: //RTS
+            case 0x08: //PHP
+            case 0x28: //PLP
+            case 0x48: //PHA
+            case 0x68: //PLA
+            case 0x88: //DEY
+            case 0xA8: //TAY
+            case 0xC8: //INY
+            case 0xE8: //INX
+            case 0x18: //CLC
+            case 0x38: //SEC
+            case 0x58: //CLI
+            case 0x78: //SEI
+            case 0x98: //TYA
+            case 0xB8: //CLV
+            case 0xD8: //CLD
+            case 0xF8: //SED
+            case 0x8A: //TXA
+            case 0x9A: //TXS
+            case 0xAA: //TAX
+            case 0xBA: //TSX
+            case 0xCA: //DEX
+            case 0xEA: //NOP
+            default:
+                if ((inputreg.getValue() & 0b00011111) == 0b10000) {
+                    char xstuff = inputreg.getValue() >> 6;
+                    bool ystuff = (inputreg.getValue() & 0b00100000) >> 5;
 
-        char aaa = (inputreg.getValue() & 0b11100000) >> 5;
-        char bbb = (inputreg.getValue() & 0b00011100) >> 2; //addressing mode
-        char cc = inputreg.getValue() & 0b00000011;
+                    bool status;
 
-        switch (cc) {
-            case 0b01:
-                switch (aaa) {
-                    case 0b000:  //ORA
-                    case 0b001:  //AND
-                    case 0b010:  //EOR
-                    case 0b011:  //ADC
-                    case 0b100:  //STA
-                    case 0b101:  //LDA
-                        a->setValue(address(bbb));
-                        break;
-                    case 0b110:  //CMP
-                    case 0b111:  //SBC
+                    switch (xstuff) {
+                        case 0b00: //negative
+                            status = inputreg.getValue() >> 7;
+                        case 0b01: //overflow
+                            status = (inputreg.getValue() >> 6) & 1;
+                        case 0b10: //carry
+                            status = inputreg.getValue() & 1;
+                        case 0b11: //zero
+                            status = (inputreg.getValue() >> 1) & 1;
+                    }
+
+                    if (status == ystuff) {
+                        //BRANCH
+                    }
                 }
-                break;
-            case 0b10:
-                switch (bbb) {
-                    case 0b000:
-                        bbb = I;
+
+                
+
+                char aaa = (inputreg.getValue() & 0b11100000) >> 5;
+                char bbb = (inputreg.getValue() & 0b00011100) >> 2; //addressing mode
+                char cc = inputreg.getValue() & 0b00000011;
+
+                switch (cc) {
+                    case 0b01:
+                        switch (aaa) {
+                            case 0b000:  //ORA
+                            case 0b001:  //AND
+                            case 0b010:  //EOR
+                            case 0b011:  //ADC
+                            case 0b100:  //STA
+                            case 0b101:  //LDA
+                                a->setValue(address(bbb));
+                                break;
+                            case 0b110:  //CMP
+                            case 0b111:  //SBC
+                        }
                         break;
-                    case 0b010:
-                        bbb = A;
+                    case 0b10:
+                        switch (bbb) {
+                            case 0b000:
+                                bbb = I;
+                                break;
+                            case 0b010:
+                                bbb = A;
+                                break;
+                        }
+                        switch (aaa) {
+                            case 0b000:  //ASL
+                            case 0b001:  //ROL
+                            case 0b010:  //LSR
+                            case 0b011:  //ROR
+                            case 0b100:  //STX
+                                if(bbb == 0b101) {bbb = ZPY;}
+                                break;
+                            case 0b101:  //LDX
+                                if(bbb == 0b101) {bbb = ZPY;}
+                                if(bbb == 0b111) {bbb = ABY;}
+                                break;
+                            case 0b110:  //DEC
+                            case 0b111:  //INC
+                        }
                         break;
-                }
-                switch (aaa) {
-                    case 0b000:  //ASL
-                    case 0b001:  //ROL
-                    case 0b010:  //LSR
-                    case 0b011:  //ROR
-                    case 0b100:  //STX
-                        if(bbb == 0b101) {bbb = ZPY;}
+                    case 0b00:
+                        if (bbb == 000) {bbb = I;}
+                        switch (aaa) {
+                            case 0b001:  //BIT
+                            case 0b010:  //JMP
+                            case 0b011:  //JMP (abs)
+                            case 0b100:  //STY
+                            case 0b101:  //LDY
+                            case 0b110:  //CPY
+                            case 0b111:  //CPX
+                        }
                         break;
-                    case 0b101:  //LDX
-                        if(bbb == 0b101) {bbb = ZPY;}
-                        if(bbb == 0b111) {bbb = ABY;}
-                        break;
-                    case 0b110:  //DEC
-                    case 0b111:  //INC
-                }
-                break;
-            case 0b00:
-                if (bbb == 000) {bbb = I;}
-                switch (aaa) {
-                    case 0b001:  //BIT
-                    case 0b010:  //JMP
-                    case 0b011:  //JMP (abs)
-                    case 0b100:  //STY
-                    case 0b101:  //LDY
-                    case 0b110:  //CPY
-                    case 0b111:  //CPX
                 }
                 break;
         }
-
         pc->inc();
     }
 }
